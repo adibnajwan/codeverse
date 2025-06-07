ca4b3a2814a14f09b30d325697e936e566886c60
adibnajwan <adibnajwan@gmail.com>
2025-06-07 19:57:34 +0700

    feat: changes some template

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import 'react';
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import Stats from './components/Stats';
-// import About from './components/About';
-// import Pricing from './components/Pricing';
-// import Testimonials from './components/Testimonials';
-// import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Stats from './components/Stats';
+import About from './components/About';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 import TeamInfo from './components/TeamInfo';
 
 function App() {
   return (
     <div className="min-h-screen">
       <TeamInfo />
-      {/* <Navbar />
+      <Navbar />
       <Hero />
       <Stats />
       <About />
       <Pricing />
       <Testimonials />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
